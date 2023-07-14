0b5e6ea0e6e32e8b2ece46d95872ac85b2da7683
FranMF8 <cejita678@gmail.com>
2023-07-14 17:23:05 -0300

    Add basic front crud

--- Angular-Promises/src/app/app.component.ts
+++ Angular-Promises/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Student } from "./models/student";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Promises';
+studentArray: Student[] = [
+  {id: 1, name: 'Francisco', dni: 42590936, email: "cejita678@gmail.com"},
+  {id: 2, name: 'Ana', dni: 1234556, email: "anita@gmail.com"},
+  {id: 3, name: 'Ivan', dni: 123456789, email: "ivanchoFortin@gmail.com"}
+]
+selectedStudent: Student = new Student();
+
+addOrEdit() {
+
+  if(this.selectedStudent.id === 0) {
+    this.selectedStudent.id = this.studentArray.length + 1;
+    this.studentArray.push(this.selectedStudent);
+  }
+
+  this.selectedStudent = new Student();
+}
+
+openForEdit(student: Student) {
+  this.selectedStudent = student;
+}
+
+delete() {
+  if(confirm('Estas seguro de querer eliminar al estudiante?')) {
+    this.studentArray = this.studentArray.filter(x => x != this.selectedStudent);
+  this.selectedStudent = new Student();
+  }
+}
 }
